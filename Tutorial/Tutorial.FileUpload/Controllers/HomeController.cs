094a14b9af46f6527f39fd9e34f3ca231dc50d99
Amogh Gupta <gupta.amogh15@gmail.com>
2017-04-11 23:24:32 +0530

    Here is an MVC form where I learned how to POST file uploaded by user in a form. I am getting the file data through the service created and then storing the file to DB.
    Its really cool. Day went well :)

---
+++ Tutorial/Tutorial.FileUpload/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tutorial.FileUpload.Models;
+
+namespace Tutorial.FileUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SaveTutorial(TutorialModel tutorial)
+        {
+            
+
+            BinaryReader br = new BinaryReader(tutorial.Attachment.InputStream);
+            Byte[] bytes = br.ReadBytes(tutorial.Attachment.ContentLength);
+            br.Close();
+
+            string strQuery = "insert into tblFiles1(Title, DescriptionCol,Attachment) values (@Title,@Description, @Attachment)";
+            SqlCommand cmd = new SqlCommand(strQuery);
+            cmd.Parameters.Add("@Title", SqlDbType.VarChar).Value = tutorial.Title;
+            cmd.Parameters.Add("@Description", SqlDbType.VarChar).Value = tutorial.Description;
+            cmd.Parameters.Add("@Attachment", SqlDbType.Binary).Value = bytes;
+            InsertUpdateData(cmd);
+
+            return Json("Tutorial Saved", JsonRequestBehavior.AllowGet);
+        }
+
+
+        private Boolean InsertUpdateData(SqlCommand cmd)
+        {
+            String strConnString = System.Configuration.ConfigurationManager
+            .ConnectionStrings["conString"].ConnectionString;
+            SqlConnection con = new SqlConnection(strConnString);
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+                return false;
+            }
+            finally
+            {
+                con.Close();
+                con.Dispose();
+            }
+        }
+
+    }
+}
---
+++ Tutorial/Tutorial.FileUpload/Scripts/Application/Controllers/home.js
@@ -0,0 +1,66 @@
+﻿var myapp = angular.module("myapplication", []);
+myapp.directive("fileModel", ["$parse",function ($parse) {
+     return {
+         restrict: "A",
+         link: function (scope, element, attrs) {
+             var model = $parse(attrs.fileModel);
+             var modelSetter = model.assign;
+             element.bind("change", function () {
+                 scope.$apply(function () {
+                     modelSetter(scope, element[0].files[0]);
+                 });
+             });
+         }
+     };
+ }
+]);
+
+myapp.factory("fileUploadService", ["$q", "$http",
+ function ($q, $http) {
+     var getModelAsFormData = function (data) {
+         var dataAsFormData = new FormData();
+         angular.forEach(data, function (value, key) {
+             dataAsFormData.append(key, value);
+         });
+         return dataAsFormData;
+     };
+
+     var saveModel = function (data, url) {
+         var deferred = $q.defer();
+         $http({
+             url: url,
+             method: "POST",
+             data: getModelAsFormData(data),
+             transformRequest: angular.identity,
+             headers: {
+                 'Content-Type': undefined
+             }
+         }).success(function (result) {
+             deferred.resolve(result);
+         }).error(function (result, status) {
+             deferred.reject(status);
+         });
+         return deferred.promise;
+     };
+
+     return {
+         saveModel: saveModel
+     }
+
+ }
+]);
+
+myapp.controller("homeCtrl", ["$scope", "fileUploadService", function ($scope, fileUploadService) {
+    $scope.saveTutorial = function () {
+        var tutorial = {
+            title: $scope.title,
+            description: $scope.description,
+            attachment: $scope.attachment
+        };
+        fileUploadService.saveModel(tutorial, "/home/saveTutorial")
+         .then(function (data) {
+             console.log(data);
+         });
+    };
+}
+]);
